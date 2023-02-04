eb17d87197fbc64dd696f7233cde867fdfdd44fa
AdrianPerFer <chopon007@gmail.com>
2023-02-04 16:43:16 +0100

    Ejercicio2 updated

--- PEjercicio2/Ejercicio2.java
+++ PEjercicio2/Ejercicio2.java
@@ -1,10 +1,5 @@
 package PEjercicio2;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 /**
  * Realiza un programa que lea el fichero creado en el
  * ejercicio anterior y que muestre los números por pantalla.
